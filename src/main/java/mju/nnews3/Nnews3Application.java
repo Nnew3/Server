065299a59b9ccb3cc4d69d6229c6efa3e7e79fb5
dajinYang <didekwls0104@naver.com>
2025-03-21 19:12:22 +0900

    [init] fisrt commit

---
+++ src/main/java/mju/nnews3/Nnews3Application.java
@@ -0,0 +1,13 @@
+package mju.nnews3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nnews3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nnews3Application.class, args);
+	}
+
+}
---
+++ src/test/java/mju/nnews3/Nnews3ApplicationTests.java
@@ -0,0 +1,13 @@
+package mju.nnews3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nnews3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
