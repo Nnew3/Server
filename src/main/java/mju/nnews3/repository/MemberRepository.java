f0525587ae3c6aaba2e4966dbabb2e51bd9ed2a7
dajinYang <didekwls0104@naver.com>
2025-04-08 22:20:45 +0900

    [feat] Member 엔티티, 레포지토리 생성

---
+++ src/main/java/mju/nnews3/repository/MemberRepository.java
@@ -0,0 +1,9 @@
+package mju.nnews3.repository;
+
+import mju.nnews3.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+}
