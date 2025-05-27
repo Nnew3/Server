cdadb0e843cfd9083e35a76b49c7bc44afd38777
dajinYang <didekwls0104@naver.com>
2025-05-28 00:10:48 +0900

    [chore] json이름 변경

--- src/main/java/mju/nnews3/config/FirebaseConfig.java
+++ src/main/java/mju/nnews3/config/FirebaseConfig.java
@@ -1,10 +1,12 @@
 package mju.nnews3.config;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
 import jakarta.annotation.PostConstruct;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
-import java.io.FileInputStream;
 import java.io.InputStream;
 
 @Configuration
@@ -12,17 +14,16 @@ public class FirebaseConfig {
     @PostConstruct
     public void init() {
         try{
-            InputStream serviceAccount = new ClassPathResource("worryboxFirebaseKey.json").getInputStream();
+            InputStream serviceAccount = new ClassPathResource("firebase-service-account.json").getInputStream();
             FirebaseOptions options = new FirebaseOptions.Builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                     .build();
 
-            if (FirebaseApp.getApps().isEmpty()) { // FirebaseApp이 이미 초기화되어 있지 않은 경우에만 초기화 실행
+            if (FirebaseApp.getApps().isEmpty()) {
                 FirebaseApp.initializeApp(options);
             }
         } catch (Exception e){
             e.printStackTrace();
         }
-    }
     }
 }
